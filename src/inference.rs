90abab1ee5c8a0da91f8252ec3ba046c3d357cd5
Bui Duc Long <longlom@yandex-team.ru>
2023-10-27 16:45:54 +0300

    BURN: working model

--- src/inference.rs
+++ src/inference.rs
@@ -1,5 +1,10 @@
+use std::collections::HashMap;
+
 use crate::{data::MNISTBatcher, training::TrainingConfig};
-use burn::data::dataset::source::huggingface::MNISTItem;
+use burn::data::dataset::source::huggingface::{MNISTDataset, MNISTItem};
+use burn::data::dataset::Dataset;
+use burn::record::Record;
+use burn::tensor::{Element, ElementConversion, Int, Tensor};
 use burn::{
     config::Config,
     data::dataloader::batcher::Batcher,
@@ -8,7 +13,7 @@ use burn::{
     tensor::backend::Backend,
 };
 
-pub fn infer<B: Backend>(artifact_dir: &str, device: B::Device, item: MNISTItem) {
+pub fn infer<B: Backend>(artifact_dir: &str, device: B::Device, items: MNISTDataset) {
     let config =
         TrainingConfig::load(format!("{artifact_dir}/config.json")).expect("A config exists");
     let record = CompactRecorder::new()
@@ -16,12 +21,36 @@ pub fn infer<B: Backend>(artifact_dir: &str, device: B::Device, item: MNISTItem)
         .expect("Failed to load trained model");
 
     let model = config.model.init_with::<B>(record).to_device(&device);
-
-    let label = item.label;
     let batcher = MNISTBatcher::new(device);
-    let batch = batcher.batch(vec![item]);
-    let output = model.forward(batch.images);
-    let predicted = output.argmax(1).flatten::<1>(0, 1).into_scalar();
 
-    println!("Predicted {} Expected {}", predicted, label);
+    const ERROR_KEY: &str = "error";
+    const OK_KEY: &str = "ok";
+    let mut stats = HashMap::new();
+    stats.insert(ERROR_KEY, 0);
+    stats.insert(OK_KEY, 0);
+
+    for i in 0..items.len() {
+        let item = items.get(i).unwrap();
+        let label = item.label;
+        let batch = batcher.batch(vec![item]);
+        let output = model.forward(batch.images);
+        let predicted = output.argmax(1).flatten::<1>(0, 1);
+        let label = Tensor::<B, 1, Int>::from_data([(label as i64).elem()]);
+
+        // println!("Predicted {} Expected {}", predicted, label);
+        let result = predicted.equal(label).into_data().value[0];
+        if result {
+            let cnt = stats.get_mut(&OK_KEY).unwrap();
+            *cnt += 1;
+        } else {
+            let cnt = stats.get_mut(&ERROR_KEY).unwrap();
+            *cnt += 1
+        }
+
+
+    }
+    println!("ok -  {} error {}", stats.get(&OK_KEY).unwrap(), stats.get(&ERROR_KEY).unwrap());
+    let accuracy = stats.get(&ERROR_KEY).unwrap() / items.len();
+    println!("accuracy -  {}", accuracy);
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 mod data;
 mod model;
 mod training;
-// mod inference;
+mod inference;
 
 use burn::autodiff::ADBackendDecorator;
 use burn::backend::wgpu::AutoGraphicsApi;
 use burn::backend::WgpuBackend;
+use burn::data::dataset::Dataset;
 use burn::optim::AdamConfig;
 use model::ModelConfig;
 use training::TrainingConfig;
+
+use crate::inference::infer;
 
 fn main() {
     type MyBackend = WgpuBackend<AutoGraphicsApi, f32, i32>;
@@ -16,16 +19,14 @@ fn main() {
     
     let device = burn::backend::wgpu::WgpuDevice::default();
     let artifact_dir = "./tmp/guide";
-    training::train::<MyAutodiffBackend>(
-        artifact_dir,
-        TrainingConfig::new(ModelConfig::new(10, 512), AdamConfig::new()),
-        device.clone(),
-    );
-    // inference::infer::<MyBackend>(
+    // training::train::<MyAutodiffBackend>(
     //     artifact_dir,
-    //     device,
-    //     burn::data::dataset::source::huggingface::MNISTDataset::test()
-    //         .get(42)
-    //         .unwrap(),
+    //     TrainingConfig::new(ModelConfig::new(10, 512), AdamConfig::new()),
+    //     device.clone(),
     // );
+    infer::<MyBackend>(
+        artifact_dir,
+        device,
+        burn::data::dataset::source::huggingface::MNISTDataset::test(),
+    );
 }
